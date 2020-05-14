be8a18f206af6594fbef85ffecb61e4cf4deb2df
Emmie Ohnuki <emaohn@gmail.com>
2020-05-13 22:26:34 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-website/precache-manifest.43c242034bf344f38722323786b1aee4.js"
+  "/my-website/precache-manifest.d74664ba05a6da7c067243acf132731d.js"
 );
 
 self.addEventListener('message', (event) => {
