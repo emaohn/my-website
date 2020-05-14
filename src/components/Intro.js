987aed8c84c5e8a74523e2ffab5ce601f3ace71a
Emmie Ohnuki <emaohn@gmail.com>
2020-05-13 22:22:23 -0400

    refactor + bootstrap

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import Typical from 'react-typical';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Row, Col} from 'react-bootstrap';
 
 export default function Intro() {
     return (
         <div>
-            <div class='row'>
-                <div class='col'>
-                    <h1>Hi, I'm Emmie Ohnuki.</h1>
-                    <p>
-                        I'm 
-                        <Typical
-                            loop={Infinity}
-                            wrapper="b"
-                            steps={[' a student.', 1000, 
-                            ' an iOS developer.', 1000, 
-                            ' a web developer.', 1000, 
-                            ' a programming instructor.', 1000, 
-                            ' a photographer.', 1000]}
-                        />
-                    </p>
-                </div>
-            </div>
-            <div class='row'>
-                <Button variant="outline-light" size='lg' block>LEARN MORE ABOUT ME</Button>{' '}
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Hi, I'm Emmie Ohnuki.</h1>
+                        <p>
+                            I'm 
+                            <Typical
+                                loop={Infinity}
+                                wrapper="b"
+                                steps={[' a student.', 1000, 
+                                ' an iOS developer.', 1000, 
+                                ' a web developer.', 1000, 
+                                ' a programming instructor.', 1000, 
+                                ' a photographer.', 1000]}
+                            />
+                        </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button variant="outline-light" size='lg'>LEARN MORE ABOUT ME</Button>{' '}
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
