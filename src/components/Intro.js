0de11b9571a3624ae92f24274c56f2c943660827
Emmie Ohnuki <emaohn@gmail.com>
2020-05-15 17:04:04 -0400

    setup material ui

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import Typical from 'react-typical';
+import Typical from 'react-typical'
 import { Button, Container, Row, Col} from 'react-bootstrap';
+import Typography from '@material-ui/core/Typography';
 
 export default function Intro() {
     return (
         <div>
             <Container>
                 <Row>
-                    <Col>
-                        <h1>Hi, I'm Emmie Ohnuki.</h1>
-                        <p>
+                    <Col sm='12'>
+                        <Typography variant='h1' component='h2' gutterBottom>
+                            Hi, I'm Emmie Ohnuki.
+                        </Typography>
+                        <Typography variant='h4' gutterBottom>
                             I'm 
-                            <Typical
-                                loop={Infinity}
-                                wrapper="b"
-                                steps={[' a student.', 1000, 
-                                ' an iOS developer.', 1000, 
-                                ' a web developer.', 1000, 
-                                ' a programming instructor.', 1000, 
-                                ' a photographer.', 1000]}
-                            />
-                        </p>
+                                <Typical
+                                    loop={Infinity}
+                                    wrapper="b"
+                                    steps={[' a CS student.', 1000, 
+                                    ' an iOS developer.', 1000, 
+                                    ' a web developer.', 1000, 
+                                    ' a hacker.', 1000,
+                                    ' a programming instructor instructor.', 1000, 
+                                    ' a photographer.', 1000]}
+                                />
+                        </Typography>
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
+                    <Col sm={12}>
                         <Button variant="outline-light" size='lg'>LEARN MORE ABOUT ME</Button>{' '}
                     </Col>
                 </Row>
--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-      crossorigin="anonymous"
-    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
