5db1824e41158a91704eb776668a7a1487f91fb3
Emmie Ohnuki <emaohn@gmail.com>
2020-05-15 22:39:34 -0400

    About section formatting

--- src/components/About.js
+++ src/components/About.js
@@ -9,15 +9,25 @@ export default function About() {
     return(
         <div>
             <ThemeProvider theme={theme}>
-                <Box p={15}>
-                    <Grid container spacing={5}>
-                        <Grid item md={6}>
-                            <div id='avatar'>
-                                <img src={require('../static/avatar.jpeg')}/>
-                            </div>
+                <Box py={10} px={15}>
+                    <Grid container alignItems='center'>
+                        <Grid item xs={12}>
+                            <Box mb={15}>
+                                <Typography variant='h2'>About</Typography>
+                            </Box>
                         </Grid>
-                        <Grid item md={6}>
-                            <Typography variant='h4'>About Me</Typography>
+                    </Grid>
+                    <Grid container spacing={6}>
+                        <Grid item md={4}>
+                            <Box>
+                                <img id='avatar' src={require('../static/avatar.jpeg')}/>
+                            </Box>
+                            <Box mt={5}>
+                                <Typography variant='p'>I'm a rising sophomore at University of Massachusetts Amherst double majoring in computer science and mathematical computation.</Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item md={8} justify='flex-start'>
+                            <p>HELLOOOOOO</p>
                         </Grid>
                     </Grid>
                 </Box>
