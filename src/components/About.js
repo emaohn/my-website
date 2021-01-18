95667a80461adff49ba623dc13de523fdb8170cf
Emmie Ohnuki <emaohn@gmail.com>
2021-01-17 21:46:57 -0500

    Add download resume button

--- src/components/About.js
+++ src/components/About.js
@@ -1,47 +1,49 @@
 import React from 'react'
-import {Link, Typography, Box, Container, Grid, Paper, Button} from '@material-ui/core';
+import { Link, Typography, Box, Container, Grid, Paper, Button } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from '../theme'
 import MeComponent from './MeComponent'
 import SkillGraph from './SkillGraph'
+import pdf from '../Resume.pdf'
 
 export default function About() {
-    return(
+    return (
         <div>
             <ThemeProvider theme={theme}>
                 <Box p={5} mx='auto'>
-                    <Grid container alignItems='center'>
+                    <Grid item container alignItems='center'>
                         <Grid item xs={12}>
                             <Box mb={6}>
                                 <Typography gutterBottom variant='h2'>About</Typography>
                             </Box>
                         </Grid>
                     </Grid>
-                    <Grid container spacing={3} alignItems='center' justify='center'>
-                        <Grid item xs={10} md={8} lg={3} xl={4} direction ='column' justify='center'>
-                            <Grid item>
-                                <Box item px={1}>
-                                    <img id='avatar' src={require('../static/avatar.jpeg')}/>
-                                </Box>
-                            </Grid>
-                            <Grid container item>
-                                <Box my={5} px={1} justifyContent='center'>
-                                    <Typography gutterBottom variant='subtitle1'>
-                                        <Box className='subtext'>
-                                            I major in computer science and minior in mathematical computation and computational linguistics at University of Massachusetts Amherst. I'm an ex-Google STEP intern and incoming software engineering intern at Asana. Currently, I'm a tech lead and leadership member at <Link target="_blank" rel="noopener noreferrer" href='https://buildumass.com/'>Build UMass</Link> and also an undergraduate research assistant at UMass BioNLP.
+                    <Grid item container spacing={3} alignItems='center' justify='center'>
+                        <Grid item xs={10} md={8} lg={3} xl={4} direction='column' justify='center' >
+                                <Grid item>
+                                    <Box item px={1}>
+                                        <img id='avatar' src={require('../static/avatar.jpeg')} />
+                                    </Box>
+                                </Grid>
+                                <Grid container item>
+                                    <Box my={5} px={1} justifyContent='center'>
+                                        <Typography gutterBottom variant='subtitle1'>
+                                            <Box className='subtext'>
+                                                I major in computer science and minior in mathematical computation and computational linguistics at University of Massachusetts Amherst. I'm an ex-Google STEP intern and incoming software engineering intern at Asana. Currently, I'm a tech lead and leadership member at <Link target="_blank" rel="noopener noreferrer" href='https://buildumass.com/'>Build UMass</Link> and also an undergraduate research assistant at UMass BioNLP.
                                         </Box>
-                                    </Typography>
-                                </Box>
-                            </Grid>
+                                        </Typography>
+                                        <Button onClick={() => window.open(pdf)}>Download Resume</Button>
+                                    </Box>
+                                </Grid>
                         </Grid>
                         <Grid item sm={8} lg={7} xl={6} justify='flex-start'>
-                            <MeComponent 
+                            <MeComponent
                                 img='Developer'
                             />
-                            <MeComponent 
+                            <MeComponent
                                 img='Hacker'
                             />
-                            <MeComponent 
+                            <MeComponent
                                 img='Collaborator'
                             />
                         </Grid>
